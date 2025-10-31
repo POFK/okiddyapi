c4ad65cfee295c06224bf70afb43f67f53d91f0a
TX.Mao <mtianxiang@gmail.com>
2025-10-31 13:15:19 +0800

    feat: udpate preview botton to `Preview Button` plugin

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,47 @@
-export default () => ({});
+// ./config/plugins.ts`
+const { env } = require("@strapi/utils");
+
+const clientUrl = env("CLIENT_URL");
+
+export default () => ({
+  // see https://market.strapi.io/plugins/strapi-plugin-preview-button
+  "preview-button": {
+    config: {
+      contentTypes: [
+        {
+          uid: "api::homepage.homepage",
+          draft: {
+            url: `${clientUrl}/preview`,
+            query: {
+              type: "home",
+              id: "{documentId}",
+            },
+            openTarget: "_blank",
+            copy: false,
+            //alwaysVisible: true,
+          },
+          published: {
+            url: clientUrl,
+          },
+        },
+        {
+          uid: "api::post.post",
+          draft: {
+            url: `${clientUrl}/preview`,
+            query: {
+              type: "post",
+              id: "{documentId}",
+            },
+            openTarget: "_blank",
+            copy: false,
+            //alwaysVisible: true,
+          },
+          published: {
+            url: `${clientUrl}/post/{documentId}`,
+            openTarget: "_blank",
+          },
+        },
+      ],
+    },
+  },
+});
