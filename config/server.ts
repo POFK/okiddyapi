39e32558a198647769ee493c0b1e0f4c1123c5b8
TX.Mao <mtianxiang@gmail.com>
2025-11-01 16:43:10 +0800

    fix: add MYDOMAIN env var

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: 'http://okiddyapi.pangu.datalab:1337',
+  url: env("MYDOMAIN"),
   app: {
     keys: env.array('APP_KEYS'),
   },
