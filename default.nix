113fd058f5a8370c786e1052c3c5ff25c6a659d7
TX.Mao <mtianxiang@gmail.com>
2025-11-16 16:38:02 +0800

    build: update nix config

--- default.nix
+++ default.nix
@@ -2,23 +2,32 @@
   config,
   lib,
   pkgs,
+  nodejs-version ? 22,
+  pname ? "okiddyapi",
+  version ? "0.0.0",
+  npmDepsHash ? "",
   ...
 }:
 let
-  pname = "okiddyapi";
-  version = "0.1.3";
-  npmDepsHash = "sha256-I24pThx/Pial+/jx7iMUOrk5t32+eHJ8pyhOgDD1xZs=";
-  node = pkgs.nodejs_22;
+  tag = "v${version}";
+  nodejs = (pkgs."nodejs_${toString nodejs-version}");
+  buildInputs = [
+    nodejs
+    pkgs.nodePackages_latest.pnpm
+  ];
+  nativeBuildInputs = buildInputs;
 in
 pkgs.buildNpmPackage {
-  inherit pname version npmDepsHash;
+  inherit
+    pname
+    version
+    npmDepsHash
+    buildInputs
+    nativeBuildInputs
+    ;
 
   src = ./.;
   npmLockfile = "package-lock.json"; # 或者 "package-lock.json" / "pnpm-lock.yaml"
-  # Strapi 通常需要一个构建步骤来编译 Admin UI，
-  # 即使你主要使用 API。这是 buildNpmPackage 的 build 脚本。
-  # 确保你的 package.json 中有 "build" 脚本（通常是 strapi build）。
-  npmBuildScript = "build";
 
   postInstall = ''
     mkdir -p $out/bin
@@ -30,20 +39,15 @@ pkgs.buildNpmPackage {
     echo "
         #!/usr/bin/env bash
         cd $lib
-        ${node}/bin/npm run start" > $exe
+        ${nodejs}/bin/npm run start" > $exe
   '';
 
-  # 运行时依赖，Strapi 运行需要 Node.js
-  runtimeDependencies = with pkgs; [
-    nodejs_22
-  ];
-
-  # 打包完成后，你可能需要一个包装脚本来启动服务
-  # 这将创建一个可执行的脚本 $out/bin/strapi-backend
-  # 运行 'npm start' 或 'node server.js' (取决于你的 Strapi 配置)
-  # 对于标准的 Strapi 项目，它会查找 package.json 中的 "start" 脚本
-  doJailbreak = true; # 可能需要，以防止 Node.js 内部路径引用问题
-
-  # 环境变量：Strapi 需要知道它在生产环境中运行
-  NODE_ENV = "production";
+  meta = {
+    description = "A strapi based api backend for my website";
+    changelog = "https://github.com/POFK/okiddyapi/releases/tag/${tag}";
+    homepage = "https://github.com/POFK/okiddyapi";
+    license = lib.licenses.mit;
+    maintainers = "txmao";
+    platforms = lib.platforms.all;
+  };
 }
--- flake.nix
+++ flake.nix
@@ -17,21 +17,55 @@
     flake-utils.lib.eachDefaultSystem (
       system:
       let
+        nodejs-version = 22;
+        pname = "okiddyapi";
+        version = "0.1.3";
+        npmDepsHash = "sha256-I24pThx/Pial+/jx7iMUOrk5t32+eHJ8pyhOgDD1xZs=";
         pkgs = nixpkgs.legacyPackages.${system};
         lib = pkgs.lib;
         strapiApp = pkgs.callPackage ./. {
-          inherit pkgs lib;
+          inherit
+            pkgs
+            lib
+            nodejs-version
+            pname
+            version
+            npmDepsHash
+            ;
         };
 
       in
       {
-        packages.okiddyapi = strapiApp;
         defaultPackage = strapiApp;
+        packages.okiddyapi = strapiApp;
+
+        # Note: Don not use it, use the Dockerfile. The following docker
+        # image is not optimized
+        # The .#docker-image package for 'nix build'. This makes sense if the
+        # flake provides only one package or there is a clear "main"
+        # package.
+        packages.docker-image = pkgs.dockerTools.buildImage {
+          name = pname;
+          tag = "v${version}";
+          copyToRoot = pkgs.buildEnv {
+            name = "image-root";
+            paths = [
+              self.packages.${system}.okiddyapi
+              pkgs.bash
+              pkgs.coreutils-full
+            ];
+            pathsToLink = [
+              "/bin"
+              "/lib"
+              "/share"
+            ];
+          };
+        };
 
         # 可选：一个开发 shell，包含 Node.js、yarn/npm 等工具
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
-            nodejs_22
+            (pkgs."nodejs_${toString nodejs-version}")
             yarn
             pnpm
           ];
